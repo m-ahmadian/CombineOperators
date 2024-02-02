b18cba17742f0abff57d17b1cf899c45ffb6fe31
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-02-02 16:46:11 -0500

    Add Xcode Project

---
+++ CombineOperators/CombineOperatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineOperatorsApp.swift
+//  CombineOperators
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct CombineOperatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
