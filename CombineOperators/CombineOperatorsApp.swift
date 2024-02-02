58c5f5e4dc991d0d4bb90ba84ca05d7d1acb28f5
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-02-02 18:35:52 -0500

    Add CombineLatest, Zip & Merge

--- CombineOperators/CombineOperatorsApp.swift
+++ CombineOperators/CombineOperatorsApp.swift
@@ -12,6 +12,9 @@ struct CombineOperatorsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    run()
+                }
         }
     }
 }
