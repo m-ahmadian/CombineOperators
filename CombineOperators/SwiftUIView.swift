26fdeae44d69a71a19746844d8b78e34a10f21cc
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-02-04 13:55:59 -0500

    Update

---
+++ CombineOperators/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CombineOperators
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-02-03.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
